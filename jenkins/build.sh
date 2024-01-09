8737234f50d6e9d1a6ac4c710cd1f003e160feef
chavezdev <jctecenge@gmail.com>
2024-01-09 17:28:25 -0300

    feat: added generic sh scripts for build and test

---
+++ jenkins/build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "******-Starting CI CD Pipeline tasks-******"
+#-BUILD
+echo ""
+echo "...... Build Phase Started :: Compiling Source Code :: ......"
+echo "Finished the build process succesfully"
---
+++ jenkins/test-all.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo ""
+echo "******-Starting CI CD Pipeline tasks-******"
+#-BUILD (TEST)
+echo ""
+echo "...... Test Phase Started :: Testing via Automated Scripts :: ......"
+echo "Finished the testing process succesfully"
